386f9a9b86d4e4f5af5f35fbd06fdc8131e331b4
barre <barrenac@oregonstate.edu>
2023-07-31 12:39:24 -0700

    added routing connection

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const routes = require('./routes')
 
 const app = express();
 const PORT = process.env.port || 3001;
@@ -9,6 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 
 
 app.use(express.static('public'));
+app.use(routes);
 
 
 app.listen(PORT, () =>
