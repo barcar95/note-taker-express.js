19b1e42de557c720326ca11f521878c85416aac1
barre <barrenac@oregonstate.edu>
2023-07-31 20:30:09 -0700

    Added post method to routes

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const { log } = require('console');
 const fs = require('fs');
+const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+const { v4: uuidv4 } = require('uuid');
 
 router.get('/notes', (req, res) => {
     fs.readFile('./db/db.json', 'UTF-8', (err, data)=> {
@@ -13,6 +15,34 @@ router.get('/notes', (req, res) => {
 })
 
 // add post route
+router.post('/notes', (req, res) => {
+    // Destructuring assignment for the items in req.body
+    const { title, text } = req.body;
+  
+    // If all the required properties are present
+    if (title && text) {
+      // Variable for the object we will save
+      const newNote = {
+        title,
+        text,
+        id: uuidv4(),
+      };
+  
+      fs.writeFile('./db/test.json', JSON.stringify(newNote), (err) => {
+        console.log(err);
+      } );
+  
+      const response = {
+        status: 'success',
+        body: newNote,
+      };
+  
+      res.json(response);
+    } else {
+      res.json('Error in posting note');
+    }
+  });
 
 // bonus route is delete route
+
 module.exports = router;
